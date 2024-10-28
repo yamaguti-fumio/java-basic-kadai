86bcc6a7b49cce8d974284c3fd51d1f9933b7cfd
山口文雄 <fumiking0211y@gmail.com>
2024-10-28 22:14:35 +0900

    Add files via upload

---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,9 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+	@Override
+	public void eachIntroduce() {
+		System.out.println("好きな食べ物はりんごです");
+	}
+
+}
---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,31 @@
+package kadai_018;
+
+public abstract class Kato_Chapter18 {
+
+	// 姓を表す
+	public String familyName = "加藤";
+	// 名を表す
+	public String givenName;
+	// 住所を表す
+	public String address = "東京都中野区⚪︎×";
+
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+
+	// 共通の紹介を出力する
+	public void commonInteroduce() {
+		System.out.println("私は" + familyName + " " + givenName + "です。");
+		System.out.println("住所は" + address + "です。");
+	}
+
+	// 個別の紹介を行う抽象メソッド
+	public abstract void eachIntroduce();
+
+	// 紹介を実行するメソッド
+	public void execIntroduce() {
+		commonInteroduce();
+		eachIntroduce();
+	}
+
+}
