5d4ef701a0392588008779568f058e6a059d87b6
山口文雄 <fumiking0211y@gmail.com>
2024-12-09 20:59:39 +0900

    Delete main directory

--- main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51QTZwHJjGqwzMHXoSsumKdPVIs6ZuVzxLLckVRqMLwEOMfuvKUHbA7s9HQbRATpJlkGv5dkEWk3wvxLPOveUHQwe00ymtSRLpK');
- const paymentButton = document.querySelector('#paymentButton');
- 
- paymentButton.addEventListener('click', () => {
-   stripe.redirectToCheckout({
-     sessionId: sessionId
-   })
- });
